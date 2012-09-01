9603b768c3d9b4f5829bf5d542c104361b45d65f
Rennan Oliveira <renmcoliver@gmail.com>
2012-09-01 05:23:36 -0300

    adicionando flash notice ao efetuar login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,9 @@
+#coding: UTF-8
 class SessionsController < ApplicationController
   def create
     user = User.from_omniauth(env["omniauth.auth"])
     session[:user_id] = user.id
+    flash[:notice] = "Login efetuado com sucesso. Alô, #{user.first_name}!"
     redirect_to root_url
   end
 end
