36e59f31b877cfe55b2f8607347d77122ebbe86c
jplethier <jplethier@gmail.com>
2012-09-01 10:12:29 -0300

    começando a utilizar o i18n

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,11 @@ class SessionsController < ApplicationController
   def create
     user = User.from_omniauth(env["omniauth.auth"])
     session[:user_id] = user.id
-    flash[:notice] = "Login efetuado com sucesso. Alô, #{user.first_name}!"
+    flash[:notice] = I18n.t('messages.success_login', :name => user.first_name)
     redirect_to root_url
+  end
+
+  def destroy
+    
   end
 end
