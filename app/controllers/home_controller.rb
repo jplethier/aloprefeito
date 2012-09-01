1f8707e1838f2f47f79bcfef16bcc9c7b797856d
Rennan Oliveira <renmcoliver@gmail.com>
2012-09-01 13:09:32 -0300

    configurando gmaps4rails

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,16 +2,16 @@ class HomeController < ApplicationController
   before_filter :authenticate_user, :except => :index
 
   def index
-    @json = Attachment.all.to_gmaps4rails do |location, marker|
-      #marker.infowindow render_to_string(:partial => "/users/my_template", :locals => { :object => user})
-      marker.picture({
-                         :picture => "http://www.blankdots.com/img/github-32x32.png",
-                         :width   => 32,
-                         :height  => 32
-                     })
-      marker.title   "Title"
-      marker.sidebar "i'm the sidebar"
-      marker.json({ :id => user.id, :foo => "bar" })
-    end
+    #@json = Attachment.new(:latitude => 0, :longitude => 0).to_gmaps4rails do |location, marker|
+    #  #marker.infowindow render_to_string(:partial => "/users/my_template", :locals => { :object => user})
+    #  marker.picture({
+    #                     :width   => 32,
+    #                     :height  => 32
+    #                 })
+    #  marker.title   "Title"
+    #  marker.sidebar "i'm the sidebar"
+    #  marker.json({ :id => 1, :foo => "bar" })
+    #end
+    @json = Attachment.all.to_gmaps4rails
   end
 end
--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -10,7 +10,7 @@ class Attachment < ActiveRecord::Base
 
   scope :mappable, where(:gmaps => true)
 
-  acts_as_gmappable :latitude => 'lat', :longitude => 'lng',:process_geocoding => false
+  acts_as_gmappable :latitude => 'lat', :longitude => 'lng', :process_geocoding => false
 
   def gmaps4rails_address
   #describe how to retrieve the address from your model, if you use directly a db column, you can dry your code, see wiki
