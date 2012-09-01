d641860cc94e288dad3b0168b32820b511d4f689
jplethier <jplethier@gmail.com>
2012-09-01 00:16:10 -0300

    iniciando o app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BondeDaUff::Application.config.secret_token = '57c9cbcfc5b25f96de915c45add146384292877f02898b058896791a8fc86c860cc6e7bec2456e1e754d153fb6140d03460db1d03a1518c96d8008be6c8d0e56'
