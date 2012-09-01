7006924d861661fa4644364f4bf508ea175c879d
jplethier <jplethier@gmail.com>
2012-09-01 05:53:12 -0300

    começando testes de integracao

---
+++ spec/requests/new_complaint_page_spec.rb
@@ -0,0 +1,29 @@
+# -*- encoding : utf-8 -*-
+require 'spec_helper'
+
+describe "New Complaint Page" do
+  include Warden::Test::Helpers
+  Warden.test_mode!
+  
+  subject { page }
+
+  before do
+    # login_as FactoryGirl.create(:broker), :scope => :broker
+    visit new_complaint_path
+  end
+
+  after { Warden.test_reset! }
+
+  it { should have_content('Novo Usuário') }
+  
+  describe 'creating a complaint' do
+    it 'successfully' do
+      count = Complaint.count
+      fill_in 'user_email', :with => 'user@ofertus.com.br'
+      click_on 'submit'
+      (Complaint.count - 1).should == count
+      should have_content('Denúncia cadastrada com sucesso.')
+      should have_content('Minhas denúncias')
+    end
+  end
+end
