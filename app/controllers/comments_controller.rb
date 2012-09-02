87d68ad671492b364e71f683865319ed97da3b3e
Rennan Oliveira <renmcoliver@gmail.com>
2012-09-02 18:17:03 -0300

    removendo debugger

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,15 @@
+class CommentsController < ApplicationController
+  before_filter :authenticate_user!
+
+  def create
+    @comment = Comment.new(params[:comment])
+    @comment.commentable = Complaint.find(params[:comment][:commentable_id])
+    if @comment.save
+      flash[:success] = I18n.t('messages.success_comment_save')
+      redirect_to complaint_path(@comment.commentable)
+    else
+      flash.now[:error] = I18n.t('messages.failure_comment_save')
+      redirect_to complaint_path(@comment.commentable_id)
+    end
+  end
+end
