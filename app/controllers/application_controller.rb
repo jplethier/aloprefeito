2ebb963f03915a6620765739fc5a4096ffd603d9
jplethier <jplethier@gmail.com>
2012-09-02 20:12:10 -0300

    gambiarra para tentar fazer o webbynode direcionar para o dominio correto

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  before_filter :redirect_to_domain
   before_filter :authenticate_user!
 
   def logout
     sign_out
     flash[:success] = I18n.t('messages.logout')
     redirect_to root_path
+  end
+
+  def redirect_to_domain
+    redirect_to 'http://www.aloprefeito.com' if request.url.match('webbynode')
   end
 end
