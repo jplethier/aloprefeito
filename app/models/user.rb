f131765c4edd521ad7af532284ba693b0714b0d3
jplethier <jplethier@gmail.com>
2012-09-02 23:16:06 -0300

    diminuindo a foto do facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,7 +23,7 @@ class User < ActiveRecord::Base
     end
   end
 
-  def facebook_profile_picture(size = "normal")
+  def facebook_profile_picture(size = "small")
     begin
       FbGraph::User.fetch(uid).picture(size)
     rescue Exception
